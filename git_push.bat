327943bc818c4fb7cb9c56a3a9097155dffdb2fc
manulapulga <manupoolakkalgtalk@gmail.com>
2025-04-25 18:56:03 +0530

    Updated app

--- git_push.bat
+++ git_push.bat
@@ -1,9 +1,5 @@
 @echo off
-<<<<<<< HEAD
-cd "C:\Users\Core i5 GAMING\Desktop\Estimate Tool Using Python"
-=======
-cd "C:\Users\manup\Desktop\Estimate Tool Using Python"
->>>>>>> 7ce34948f0eaf84ddaa59896b8f7fe5e11b331a9
+cd "C:\Users\Core i5 GAMING\Desktop\estimate-drafter"
 git add .
 git commit -m "Updated app"
 git push
--- run.bat
+++ run.bat
@@ -1,8 +1,4 @@
 @echo off
-<<<<<<< HEAD
-cd /d "C:\Users\Core i5 GAMING\Desktop\Estimate Tool Using Python"
-=======
-cd /d "C:\Users\manup\Desktop\Estimate Tool Using Python"
->>>>>>> 7ce34948f0eaf84ddaa59896b8f7fe5e11b331a9
+cd /d "C:\Users\Core i5 GAMING\Desktop\estimate-drafter"
 streamlit run streamlit_app.py
 pause
