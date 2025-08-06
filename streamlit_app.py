81d1cc28adf17420afbb56b470233e512d3a41c6
manulapulga <manupoolakkalgtalk@gmail.com>
2025-08-06 21:53:23 +0530

    Auto commit 06-08-2025 21:53:23.50

--- streamlit_app.py
+++ streamlit_app.py
@@ -1450,7 +1450,7 @@ def main_app():
     # Add New Item or Subheading buttons
     button_col1, button_col2, button_col3, button_col4, button_col5, button_col6, button_col7, button_col8, button_col9  = st.columns([2, 2, 2, 2, 2, 2.2, 2, 2, 2])
     with button_col7:
-        if st.button("📊 Section Total", 
+        if st.button("🔢 Section Sum", 
                     key="add_summary_btn",
                     help="Add section total in between items"):
             st.session_state.selected_items.append({
